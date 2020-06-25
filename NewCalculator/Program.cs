05643acc886eb36ed21886551311104e1d9c54e6
Brett Andrews <brettandrews3@gmail.com>
2020-06-25 15:30:01 -0500

    New Calc code written. About to debug

--- NewCalculator/Program.cs
+++ NewCalculator/Program.cs
@@ -6,7 +6,86 @@ namespace NewCalculator
     {
         static void Main(string[] args)
         {
-            // Declare our variables and initialize to 0:
+            bool endApp = false; //Is this app over? No!
+            //First up, here's the app's title:
+            Console.WriteLine("Brett's C# Console Calculator\r");
+            Console.WriteLine("-----------------------------\n");
+
+            while (!endApp) //So long as the app is running:
+            {
+                //Toss in some empty variables. Make 'em string for flexibility:
+                string numInput1 = "";
+                string numInput2 = "";
+                double result = 0;
+
+                //Ask user for the first number
+                Console.WriteLine("Welcome! Enter your first number here, then press Enter: ");
+                numInput1 = Console.ReadLine();
+
+                //cleanNum var catches any invalid user inputs, asks again for an integer
+                double cleanNum1 = 0;
+                while (!double.TryParse(numInput1, out cleanNum1))
+                {
+                    Console.WriteLine("Invalid input. Give me an integer value, please: ");
+                    numInput1 = Console.ReadLine();
+                }
+
+                //Ask user for the second number
+                Console.WriteLine($"That first number is {numInput1}. Enter that second number here: ");
+                numInput2 = Console.ReadLine();
+
+                //cleanNum var catches any invalid user inputs, asks again for an integer
+                double cleanNum2 = 0;
+                while (!double.TryParse(numInput2, out cleanNum2))
+                {
+                    Console.WriteLine($"{cleanNum2} is invalid input. Give an integer value, please: ");
+                    numInput2 = Console.ReadLine();
+                }
+
+                //Ask the user for which operator they want to use:
+                Console.WriteLine("-----------------------------\n");
+                Console.WriteLine("Alright! Now, choose an operator from this list:"):
+                Console.WriteLine("\ta - Add");
+                Console.WriteLine("\ts - Subtract");
+                Console.WriteLine("\tm - Multiply");
+                Console.WriteLine("\td - Divide");
+                Console.WriteLine("\to - Modulus");
+                Console.Write("Which one do you choose?"):
+
+                string op = Console.ReadLine();
+
+                try
+                {
+                    result = Calculator.Do0peration(cleanNum1, cleanNum2, op);
+                    if (double.IsNaN(result))
+                    {
+                        Console.WriteLine("This operation results in a mathematical error. \n");
+                    }
+                    else Console.WriteLine("Your result: {0:0.##}\n", result);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Oops! An exception popped up while doing the math. \n" +
+                        " - Details: " + e.Message);
+                }
+
+                Console.WriteLine("-----------------------------\n");
+
+                //Wait for user input before closing the app
+                Console.Write("Nice job. Press 'n' and Enter to close the app, or press" +
+                    "any other key and Enter to go again: ");
+                if (Console.ReadLine() == "n") endApp = true;
+
+                Console.WriteLine("\n"); //A little friendly line spacing
+            }
+            return; //Now, let's take it from the top!
+        }
+
+    }
+}
+
+/* Original code:
+ * {            // Declare our variables and initialize to 0:
             double num1 = 0; double num2 = 0;
 
             //Display title of the console app:
@@ -67,6 +146,4 @@ namespace NewCalculator
             Console.WriteLine("------------------");
             Console.Write("Excellent! Press any key to close the C# Console Calculator app.");
             Console.ReadKey();
-        }
-    }
-}
+        } */
