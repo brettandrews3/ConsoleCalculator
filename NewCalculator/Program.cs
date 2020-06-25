c4d89d2af49e0682c35937b716c3590cc53ab4ed
Brett Andrews <brettandrews3@gmail.com>
2020-06-25 13:57:19 -0500

    Asks user for 2 numbers

---
+++ NewCalculator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace NewCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare our variables and initialize to 0:
+            int num1 = 0; int num2 = 0;
+
+            //Display title of the console app:
+            Console.WriteLine("Brett's C# Console Calculator\r");
+            Console.WriteLine("-----------------------------\n");
+
+            //Ask user to type their first number:
+            Console.WriteLine("Enter a number here, then press Enter:");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            //Ask user for the second number:
+            Console.WriteLine($"Your first number is {num1}. What's your second number?");
+            num2 = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
